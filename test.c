f938c8a48096aee0ba1f280b4b62971f4b823292
Chareeef <youssef.charif.h@gmail.com>
2023-10-31 17:56:19 +0100

    Add 12-binary_tree_leaves.c

--- test.c
+++ test.c
@@ -7,25 +7,26 @@
  */
 int main(void)
 {
-    binary_tree_t *root;
-    size_t size;
+	    binary_tree_t *root;
+	    size_t leaves;
 
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    binary_tree_insert_right(root->left, 54);
-    binary_tree_insert_right(root->left->right, 549);
-    binary_tree_insert_left(root->left->right->right, 49);
-    binary_tree_insert_right(root, 128);
-    binary_tree_print(root);
+	    root = binary_tree_node(NULL, 98);
+	    root->left = binary_tree_node(root, 12);
+	    root->right = binary_tree_node(root, 402);
+	    binary_tree_insert_right(root->left, 54);
+	    binary_tree_insert_right(root->left->right, 549);
+	    binary_tree_insert_left(root->left->right, 9);
+	    binary_tree_insert_left(root->left->right->right, 49);
+	    binary_tree_insert_right(root, 128);
+	    binary_tree_print(root);
 
-    size = binary_tree_size(root);
-    printf("size from %d: %u\n", root->n, size);
-    size = binary_tree_size(root->right);
-    printf("size from %d: %u\n", root->right->n, size);
-    size = binary_tree_size(root->left->right);
-    printf("size from %d: %u\n", root->left->right->n, size);
-    size = binary_tree_size(root->left->right->right->left);
-    printf("size from %d: %u\n", root->left->right->right->left->n, size);
-    return (0);
+	    leaves = binary_tree_leaves(root);
+	    printf("leaves from %d: %u\n", root->n, leaves);
+	    leaves = binary_tree_leaves(root->right);
+	    printf("leaves from %d: %u\n", root->right->n, leaves);
+	    leaves = binary_tree_leaves(root->left->right);
+	    printf("leaves from %d: %u\n", root->left->right->n, leaves);
+	    leaves = binary_tree_leaves(root->left->right->right->left);
+	    printf("leaves from %d: %u\n", root->left->right->right->left->n, leaves);
+	    return (0);
 }
