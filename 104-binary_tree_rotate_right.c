dd6f3fea28a34c4c5cc48e87ec0fa2f60706fda5
Chareeef <youssef.charif.h@gmail.com>
2023-11-01 17:39:55 +0100

    Add 104-binary_tree_rotate_right.c

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_right - Performs a right-rotation on a binary tree
+ * @tree: Pointer to the root node of the tree to right-rotate
+ *
+ * Return: A pointer to the new root node of the tree once rotated.
+ */
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	binary_tree_t *new_root;
+	binary_tree_t *temp;
+
+	if (!tree || !tree->left)
+		return (tree);
+
+	new_root = tree->left;
+
+	tree->parent = new_root;
+	new_root->parent = NULL;
+
+	temp = new_root->right;
+	new_root->right = tree;
+	tree->left = temp;
+
+	if (temp)
+		temp->parent = tree;
+
+	return (new_root);
+}
--- test.c
+++ test.c
@@ -6,47 +6,24 @@
  * @n1: First node
  * @n2: Second node
  */
-void launch_test(binary_tree_t *n1, binary_tree_t *n2)
-{
-	binary_tree_t *ancestor;
-
-	ancestor = binary_trees_ancestor(n1, n2);
-	if (n1 && n2)
-	    printf("Ancestor of [%d] & [%d]: ", n1->n, n2->n);
-	if (!ancestor)
-	    printf("(nil)\n");
-	else
-	    printf("%d\n", ancestor->n);
-}
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
 	binary_tree_t *root;
-	binary_tree_t *root2;
 
 	root = binary_tree_node(NULL, 98);
-	root->left = binary_tree_node(root, 12);
-	root->right = binary_tree_node(root, 402);
-	root->left->right = binary_tree_node(root->left, 54);
-	root->right->right = binary_tree_node(root->right, 128);
-	root->left->left = binary_tree_node(root->left, 10);
-	root->right->left = binary_tree_node(root->right, 45);
-	root->right->right->left = binary_tree_node(root->right->right, 92);
-	root->right->right->right = binary_tree_node(root->right->right, 65);
+	root->left = binary_tree_node(root, 64);
+	root->left->left = binary_tree_node(root->left, 32);
 	binary_tree_print(root);
+	printf("Rotate-right %d\n", root->n);
+	root = binary_tree_rotate_right(root);
+	binary_tree_print(root);
+	printf("\n");
 
-	root2 = binary_tree_node(NULL, 9569);
-	launch_test(root, root);
-	launch_test(root, root2);
-	launch_test(root->left, root->right);
-	launch_test(root->right->left, root->right->right->right);
-	launch_test(root->right->right, root->right->right->right);
-	launch_test(root->right->right, root->right->right);
-	launch_test(NULL, root);
+	root->left->left = binary_tree_node(root->left, 20);
+	root->left->right = binary_tree_node(root->left, 56);
+	binary_tree_print(root);
+	printf("Rotate-right %d\n", root->n);
+	root = binary_tree_rotate_right(root);
+	binary_tree_print(root);
 	return (0);
 }
