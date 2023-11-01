41407acd9c93467641dd988400b799f0687b0d66
Omarr-kh <omar1752003@gmail.com>
2023-10-31 23:57:34 -0700

    bug fixed for task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,37 +14,20 @@ int binary_tree_balance(const binary_tree_t *tree)
 }
 
 /**
- * recursive_height - Recursively compute the binary tree's height
- * @node: current node
- * @cumulative_h: cumulative height
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the height.
  *
- * Return: The height of the binary tree
- */
-size_t recursive_height(const binary_tree_t *node, size_t cumulative_h)
-{
-	size_t left_h, right_h;
-
-	if (!node)
-		return (cumulative_h - 1);
-
-	left_h =  recursive_height(node->left, cumulative_h + 1);
-	right_h = recursive_height(node->right, cumulative_h + 1);
-
-	cumulative_h = left_h > right_h ? left_h : right_h;
-
-	return (cumulative_h);
-}
-
-/**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree to measure the height
- *
- * Return: The height of the binary tree, or 0 if tree is NULL or has no child
+ * Return: If tree is NULL, your function must return 0, else return height.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+	if (tree)
+	{
+		size_t l_height = 0, r_height = 0;
 
-	return (recursive_height(tree, 0));
+		l_height = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r_height = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((l_height > r_height) ? l_height : r_height);
+	}
+	return (0);
 }
