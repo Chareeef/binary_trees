546a635be2d1bd811d82ae53b75bec1056ac06b0
Omarr-kh <omar1752003@gmail.com>
2023-11-02 04:12:41 -0700

    task 113

---
+++ 113-bst_search.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * bst_search - searches for a value in a BST
+ * @tree: pointer to the node
+ * @value: value to look for
+ *
+ * Return: pointer to the node containing the value
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (tree)
+	{
+		if (tree->n > value)
+			return (bst_search(tree->left));
+		else if (tree->n < value)
+			return (bst_search(tree->right));
+		else
+			return (tree);
+	}
+	return (NULL);
+}
