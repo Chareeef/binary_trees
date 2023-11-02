b7f404fbe3f0e2e8698e9d9937158e262af0663f
Omarr-kh <omar1752003@gmail.com>
2023-11-02 04:12:56 -0700

    prototype for task 113 added

--- binary_trees.h
+++ binary_trees.h
@@ -69,6 +69,7 @@ binary_tree_t	*binary_tree_rotate_left(binary_tree_t *tree);
 bst_t		*bst_insert(bst_t **tree, int value);
 avl_t		*avl_insert(avl_t **tree, int value);
 bst_t		*array_to_bst(int *array, size_t size);
+bst_t		*bst_search(const bst_t *tree, int value);
 
 /* Helper functions */
 
