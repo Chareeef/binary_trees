4d1ced808438758e0f7604e9824d1f347a59deaf
Omarr-kh <omar1752003@gmail.com>
2023-11-02 04:16:41 -0700

    prototype for task 28 added

--- binary_trees.h
+++ binary_trees.h
@@ -70,6 +70,7 @@ bst_t		*bst_insert(bst_t **tree, int value);
 avl_t		*avl_insert(avl_t **tree, int value);
 bst_t		*array_to_bst(int *array, size_t size);
 bst_t		*bst_search(const bst_t *tree, int value);
+bst_t		*bst_remove(bst_t *root, int value);
 
 /* Helper functions */
 
